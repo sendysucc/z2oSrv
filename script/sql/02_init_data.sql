c51681a783d9606937c91a89b143ed18ca3ca2af
sendysucc <sendysucc@gmail.com>
2019-01-01 17:13:06 +0800

    error fixed

--- script/sql/02_init_data.sql
+++ script/sql/02_init_data.sql
@@ -1,3 +1,13 @@
 use z2osrv;
 
--- insert into 
+insert into Agent(agentid,name,cellphone,createtime) value (1,'系统默认','09566014786',now());
+
+insert into Game(gameid,name,minplayers,maxplayers,enable) value (20001,'抢庄牛牛',2,6,true);
+
+insert into Promote(promoteid,code,name,detail,createtime) value(1,'sjda1001','系统默认','自身推广',now());
+
+insert into GameRoom(roomid,name,minentry,maxentry,gameid,enable) value (1,'体验房',10,100,20001,true);
+insert into GameRoom(roomid,name,minentry,maxentry,gameid,enable) value (2,'中级房',200,900,20001,true);
+insert into GameRoom(roomid,name,minentry,maxentry,gameid,enable) value (3,'高级房',1000,10000,20001,true);
+insert into GameRoom(roomid,name,minentry,maxentry,gameid,enable) value (4,'VIP房',20000,900000,20001,true);
+
--- script/sql/03_proc_register.sql
+++ script/sql/03_proc_register.sql
@@ -4,13 +4,34 @@ drop procedure if exists proc_register;
 
 delimiter ;;
 
-create procedure proc_register (IN cellphone varchar(11), IN passwd varchar(40))
+create procedure proc_register (IN cellphone varchar(11), IN passwd varchar(40), IN agentcode varchar(6),IN promotecode varchar(50))
 label_reg:begin
-
     declare errcode int;
+    declare o_agentid int;
+    declare o_promoteid int;
+    declare o_userid int;
 
+    set o_agentid = 0;
+    set o_promoteid = 0;
+    set o_userid = 0;
+
+    select agentid into o_agentid from Agent where Agent.agentid = agentcode;
+    if o_agentid <=> 0 then
+        select agentid into o_agentid from Agent where Agent.aigentid = 1;
+    end if;
+
+    select promoteid into o_promoteid from Promote where Promote.code = promotecode;
+    if o_promoteid <=> 0 then
+        select promoteid into o_promoteid from Promote where Promote.promoteid = 1;
+    end if;
+
+
+    if length(passwd) != 40 then
+        return 2;   --password error.
+    end if;
+
+    select userid into o_userid from User where User.cellphone = cellphone;
 
 
 end
-
 ;;
