09b6a95cd8adbd34041572b26b2092b6fa76aa8c
sendysucc <sendysucc@gmail.com>
2019-01-10 19:03:24 +0800

    robot progress

--- snax/manager/robotmanager.lua
+++ snax/manager/robotmanager.lua
@@ -9,13 +9,21 @@ local alreadyload = 0   --已经加载机器人数量
 
 local function loadrobots()
     local rets = snax.queryservice('dbmanager').req.loadrobots(alreadyload,loadcounts)
-    idlerobots = rets
+    for k,v in pairs(rets) do
+        table.insert(idlerobots,v)
+    end
 end
 
 function init(...)
     loadrobots()
 end
 
-function response.newrobot()
+function response.getarobot()
+    if #idlerobots <= 0 then
+        loadrobots()
+    end
 
+    local robot = table.remove(idlerobots, math.random(1,#idlerobots))
+    table.insert(busyrobots,robot)
+    return robot
 end
